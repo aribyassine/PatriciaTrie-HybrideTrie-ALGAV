53d42b224ab6d5876d82105357579267b51bdfe3
aribyassine <aribyassine@hotmail.fr>
2016-11-19 04:18:14 +0100

    liste mots + hauteur + comptage mot + recherche

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,41 @@ import trie.PatriciaTrie;
 public class Main {
 
     public static void main(String[] args) {
-        String[] tab = {"aaa","a","aa","aaaa","a"};
-        String[] tab2 = {"romane","romanus","romulus","rubens","ruber","rubicon","rubicundus"};
-        PatriciaTrie pt = new PatriciaTrie("a", new PatriciaTrie("a", new PatriciaTrie("a", new PatriciaTrie())));
-
-        System.out.println(pt);
-        pt = new PatriciaTrie();
-        for (String s : tab2) {
+        String[] tab = {"a","ab","abb",""};
+        String[] tab2 = {"romane", "romanus", "romulus", "rubens", "ruber", "rubicon", "rubicundus", "rubic"};
+        String exemple = "A quel genial professeur de dactylographie sommes nous redevables de la superbe phrase ci dessous, un modele du genre, que toute dactylo connait par coeur puisque elle fait appel a chacune des touches du clavier de la machine a ecrire ?";
+        String[] tab3 = exemple.split(" ");
+        PatriciaTrie pt = new PatriciaTrie();
+        for (String s : tab) {
             pt.ajouter(s);
-            System.out.println(pt);
         }
+        System.out.println(pt);
+        System.out.println(pt.comptageMots());
+        System.out.println(pt.hauteur());
+        System.out.println(pt.listeMots());
 
         /*
-        System.out.println("-->"+pt.contientMot("a"));
-        System.out.println("-->"+pt.contientMot("aa"));
-        System.out.println("-->"+pt.contientMot("aaa"));
-        System.out.println("-->"+pt.contientMot(""));
-        System.out.println("-->"+pt.contientMot("lol"));
-        System.out.println("-->"+pt.contientMot("aaaa"));
+        System.out.println("_______________________");
+        System.out.println(pt.sousArbre(0));
+        System.out.println(pt.sousArbre(0).sousArbre(0));
+        System.out.println(pt.sousArbre(0).sousArbre(0).sousArbre(0));
+        System.out.println(pt.sousArbre(0).sousArbre(0).sousArbre(0).sousArbre(0));
+        System.out.println("_______________________");
+        System.out.println(pt.sousArbre(0).niveau());
+        System.out.println(pt.sousArbre(0).sousArbre(0).niveau());
+        System.out.println(pt.sousArbre(0).sousArbre(0).sousArbre(0).niveau());
+        System.out.println(pt.sousArbre(0).sousArbre(0).sousArbre(0).sousArbre(0).niveau());*/
+
+
+        //pt.sousArbre(0).sousArbre(0).sousArbre(0);
+
+        /*
+        System.out.println("-->"+pt.recherche("a"));
+        System.out.println("-->"+pt.recherche("aa"));
+        System.out.println("-->"+pt.recherche("aaa"));
+        System.out.println("-->"+pt.recherche(""));
+        System.out.println("-->"+pt.recherche("lol"));
+        System.out.println("-->"+pt.recherche("aaaa"));
         System.out.println("______________________________________________");
         System.out.println("-->"+pt.contientMotNonStricte("a"));
         System.out.println("-->"+pt.contientMotNonStricte("aa"));
--- src/trie/PatriciaTrie.java
+++ src/trie/PatriciaTrie.java
@@ -4,38 +4,34 @@ import java.util.*;
 
 public class PatriciaTrie {
     private HashMap<String, PatriciaTrie> cle;
+    private PatriciaTrie pere;
+    public static String finMot = "є";
 
-    public PatriciaTrie(String mot) {
-        this.cle = new HashMap<String, PatriciaTrie>();
-        this.cle.put(mot, new PatriciaTrie());
-    }
 
     public PatriciaTrie() {
         this.cle = new HashMap<String, PatriciaTrie>();
-
     }
 
-    public PatriciaTrie(String mot, PatriciaTrie cle) {
+    private PatriciaTrie(String mot, PatriciaTrie cle) {
         this.cle = new HashMap<String, PatriciaTrie>();
         this.cle.put(mot, cle);
     }
+
 
     public boolean estVide() {
         return this.cle.isEmpty();
     }
 
-    public Set<String> valeurRacine() {
-        return this.cle.keySet();
-    }
-
-    public Collection<PatriciaTrie> sousArbres() {
-        return this.cle.values();
-    }
-
-    public String sousArbre(int i) {
+    public List<String> valeursRacine() {
         List<String> values = new ArrayList<String>(this.cle.keySet());
         Collections.sort(values);
-        return values.get(i);
+        return values;
+    }
+
+    public PatriciaTrie sousArbre(int i) {
+        List<String> values = new ArrayList<String>(this.cle.keySet());
+        Collections.sort(values);
+        return this.cle.get(values.get(i));
     }
 
     public static int longueurPlusGrandPrefixeCommun(String a, String b) {
@@ -48,62 +44,123 @@ public class PatriciaTrie {
         return len;
     }
 
-
     private String prefixe(String a, String b) {
         return a.substring(0, longueurPlusGrandPrefixeCommun(a, b));
     }
 
     public void ajouter(String mot) {
-        if (this.estVide())
-            cle.put(mot, new PatriciaTrie());
-        else {
-            PatriciaTrie tmp;
-            int lp = 0;
-            for (String s : this.valeurRacine()) {
-                lp = longueurPlusGrandPrefixeCommun(s, mot);
-                if (lp != 0) {
-                    String prefixe = prefixe(s, mot);
-                    if (!(lp == s.length())) {
-                        tmp = cle.get(s);
-                        cle.remove(s);
-                        cle.put(prefixe, new PatriciaTrie(s.substring(lp), tmp));
+        if (!mot.isEmpty()) {
+            if (this.estVide()) {
+                PatriciaTrie pt = new PatriciaTrie();
+                cle.put(mot, pt);
+                pt.cle.put(finMot, new PatriciaTrie());
+                pt.setPere(this);
+            } else {
+                PatriciaTrie tmp;
+                int lp = 0;
+                for (String s : this.valeursRacine()) {
+                    lp = longueurPlusGrandPrefixeCommun(s, mot);
+                    if (lp != 0) {
+                        String prefixe = prefixe(s, mot);
+                        if (!(lp == s.length())) {
+                            tmp = cle.get(s);
+                            cle.remove(s);
+                            PatriciaTrie pt = new PatriciaTrie(s.substring(lp), tmp);
+                            cle.put(prefixe, pt);
+                            pt.setPere(this);
+                            tmp.setPere(pt);
+                        }
+                        if (!(lp == mot.length()))
+                            cle.get(prefixe).ajouter(mot.substring(lp));
+                        else
+                            cle.get(prefixe).cle.put(finMot, new PatriciaTrie());
+                        break;
                     }
-                    cle.get(prefixe).ajouter(mot.substring(lp));
-                    break;
                 }
-            }
-            if (lp == 0) {
-                cle.put(mot, new PatriciaTrie());
+                if (lp == 0) {
+                    PatriciaTrie pt = new PatriciaTrie();
+                    cle.put(mot, pt);
+                    pt.cle.put(finMot, new PatriciaTrie());
+                    pt.setPere(this);
+                }
             }
         }
     }
 
     private boolean hasFinMot() {
-        for (String s : valeurRacine()) {
-            if (s.isEmpty())
+        for (String s : valeursRacine()) {
+            if (s.equals(finMot))
                 return true;
         }
         return false;
     }
 
-    public boolean contientMot(String mot) {
+    public boolean recherche(String mot) {
         if (mot.isEmpty() && (this.hasFinMot() || this.estVide()))
             return true;
         int lp = 0;
-        for (String s : this.valeurRacine()) {
+        for (String s : this.valeursRacine()) {
             lp = longueurPlusGrandPrefixeCommun(s, mot);
             if (lp != 0 && lp == s.length()) {
-                return cle.get(s).contientMot(mot.substring(lp));
+                return cle.get(s).recherche(mot.substring(lp));
             }
         }
+
         return false;
+    }
+
+    public int hauteur() {
+        int hMax = 0;
+        if (this.cle.isEmpty())
+            return 0;
+        for (PatriciaTrie pt : cle.values()) {
+            int hFils = pt.hauteur();
+            if (hFils > hMax)
+                hMax = hFils;
+        }
+        return hMax + 1;
+    }
+
+    public List<String> listeMots() {
+        ArrayList<String> mots = new ArrayList<String>();
+        for (StringBuilder sb : this.listeMotsRecursive()) {
+            mots.add(sb.toString());
+        }
+        Collections.sort(mots);
+        return mots;
+    }
+
+    private ArrayList<StringBuilder> listeMotsRecursive() {
+        ArrayList<StringBuilder> res = new ArrayList<StringBuilder>();
+        for (String s : cle.keySet()) {
+            //si feille on ajoute un mot vide
+            if (s.equals(finMot))
+                res.add(new StringBuilder());
+            else {
+                ArrayList<StringBuilder> suffixes = cle.get(s).listeMotsRecursive();
+                // on concat le mot courant et les suffixes et on ajoute a la liste
+                for (StringBuilder suffixe : suffixes) {
+                    res.add(new StringBuilder(s).append(suffixe));
+                }
+            }
+        }
+        return res;
+    }
+
+    public int comptageMots() {
+        int nbMot = 0;
+        if (cle.keySet().contains(finMot))
+            nbMot++;
+        for (PatriciaTrie pt : cle.values())
+            nbMot += pt.comptageMots();
+        return nbMot;
     }
 
     public boolean contientMotNonStricte(String mot) {
         if (mot.isEmpty())
             return true;
         int lp = 0;
-        for (String s : this.valeurRacine()) {
+        for (String s : this.valeursRacine()) {
             lp = longueurPlusGrandPrefixeCommun(s, mot);
             if (lp != 0) {
 /*                System.out.println("mot "+mot);
@@ -113,6 +170,16 @@ public class PatriciaTrie {
             }
         }
         return false;
+    }
+
+    public int niveau() {
+        int lvl = 0;
+        PatriciaTrie pere = this.pere;
+        while (pere != null) {
+            pere = pere.getPere();
+            lvl++;
+        }
+        return lvl;
     }
 
     public boolean supprimer(String mot) {
@@ -129,12 +196,28 @@ public class PatriciaTrie {
         return true;
     }
 
+    public PatriciaTrie getPere() {
+        return pere;
+    }
+
+    public void setPere(PatriciaTrie pere) {
+        this.pere = pere;
+    }
+
     public boolean isLeaf() {
         return this.cle.isEmpty();
     }
 
     @Override
     public String toString() {
-        return cle.toString();
+        StringBuilder sb = new StringBuilder();
+        for (String s : this.valeursRacine()) {
+            for (int i = 0; i < this.niveau(); i++) {
+                sb.append("|\t");
+            }
+            sb.append("|-->" + s + "\n").append(this.cle.get(s).toString());
+        }
+        return sb.toString();
+        //return this.cle.toString();
     }
 }
